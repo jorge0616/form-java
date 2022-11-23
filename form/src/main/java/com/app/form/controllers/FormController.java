b40ca8d8f80d617e03f4469e8b3dda8ccb61a4a1
jorge0616 <jlozano299@mgial.com>
2022-11-22 21:44:22 -0500

    agregando plantillas

--- form/src/main/java/com/app/form/controllers/FormController.java
+++ form/src/main/java/com/app/form/controllers/FormController.java
@@ -4,17 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
-    
+
     @GetMapping("/form")
-    public String form(Model model){
+    public String form(Model model) {
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model){
+    public String procesar(Model model,
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam String email) {
+        
+        model.addAttribute("titulo", "Formulario Uusarios");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
         return "resultado";
     }
 
