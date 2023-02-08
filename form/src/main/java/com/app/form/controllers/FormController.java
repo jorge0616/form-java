699207443ce0ca689379d2be29f30991c6aab7ce
jorge0616 <jlozano299@mgial.com>
2023-02-07 20:40:51 -0500

    Agregando validación

--- form/src/main/java/com/app/form/controllers/FormController.java
+++ form/src/main/java/com/app/form/controllers/FormController.java
@@ -1,29 +1,44 @@
 package com.app.form.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.app.form.model.Usuario;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("titulo", "Formulario Registro");
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model,
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestParam String email) {
-        
-        model.addAttribute("titulo", "Formulario Uusarios");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+
+        model.addAttribute("titulo", "Formulario Usuarios");
+
+        if (result.hasErrors()) {
+            
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));});
+            model.addAttribute("error", errores);
+            return "form";
+
+        }
+
+        model.addAttribute("usuario", usuario);
 
         return "resultado";
     }
